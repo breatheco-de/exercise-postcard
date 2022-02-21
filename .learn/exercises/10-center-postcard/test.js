6cf64c5213d2adce1bfe8c2d561dbaa436793723
Martín Suárez <ms.prgramador@gmail.com>
2022-02-21 16:12:00 +0000

    10-center-postcard

--- .learn/exercises/10-center-postcard/test.js
+++ .learn/exercises/10-center-postcard/test.js
@@ -3,41 +3,40 @@ const path = require("path");
 const { fromFile, fromHTML } = require("../../utils/dom");
 let dom = fromFile();
 
-test("Make sure you have your index.html", function(){
+test("Make sure you have your index.html", function () {
     const indexExists = fs.existsSync(`./index.html`);
     expect(indexExists).toBe(true);
 })
 
-test("Make sure you created the styles.css file", function(){
+test("Make sure you created the styles.css file", function () {
     const indexExists = fs.existsSync(`./styles.css`);
     expect(indexExists).toBe(true);
 })
 
-test("Make sure your index.html contains the <head> tag", function(){
+test("Make sure your index.html contains the <head> tag", function () {
     const _dom = dom.insideTags('head')
     expect(_dom).not.toBe(null);
 })
 
-test("Create a .postcard selector on your stylesheet", function(){
+test("Create a .postcard selector on your stylesheet", function () {
     dom = dom.withStylesheet();
     dom.selector('.postcard')
 })
 
-test("Add the background, width, height and margin CSS rules to your class", function(){
+test("Add the background, width, height and margin CSS rules to your class", function () {
     dom = dom.withStylesheet();
     let result, result2 = false
     try {
-        
+
         result = dom.selector('.postcard').hasStyles({
             background: "white",
             width: "400px",
             height: "300px",
             margin: "auto",
         })
-        
+
     } catch (error) {
         console.log(error);
-        
     }
     try {
         result2 = dom.selector('.postcard').hasStyles({
@@ -47,9 +46,8 @@ test("Add the background, width, height and margin CSS rules to your class", fun
             margin: "auto",
         })
     } catch (error) {
-        
+        console.log(error);
     }
 
-    
-    expect(result || result2).toBe(true) 
+    expect(result || result2).toBe(true)
 })
