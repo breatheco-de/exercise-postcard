a3c17d0aceb5f194a3892536c652a8503a5f7ce2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-04-19 14:48:21 +0200

    Update test.js

--- .learn/exercises/07-the-first-div/test.js
+++ .learn/exercises/07-the-first-div/test.js
@@ -20,17 +20,17 @@ test("Your <body> and or </body> tags should not be duplicated", function(){
     }).toThrow()
 })
 
-test("There should be at least one <div> with class postcard inside <body> tag and the tag body tag must be inside the <html> tag", function(){
+test("There should be at least one <div> with class postcard inside the <body> and the <body> tag must be inside the <html> tag", function(){
     const div = dom.insideTags('html').querySelector('div')
     expect(div.className.includes('postcard')).toBe(true)
 })
 
-test("Make sure there is a div with the class postcard", function(){
+test("Make sure there is a <div> with the class postcard", function(){
     const linkTag = dom.querySelector("div.postcard")
     expect(linkTag).not.toBe(null);
 })
 
-test("Make sure the div with class postcard is inside the body tags", function(){
+test("Make sure the <div> with class postcard is inside the <body> tags", function(){
     const found = dom.insideTags('body').querySelector("div.postcard")
     expect(found).not.toBe(null);
 })
