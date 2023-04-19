0941c934bddb6e2cdddc3e7baf697e9d71691319
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-04-19 16:57:01 +0200

    Update test.js

--- .learn/exercises/11.1-header-content/test.js
+++ .learn/exercises/11.1-header-content/test.js
@@ -20,22 +20,22 @@ test("Make sure your index.html contains the <link> tag", function(){
     expect(linkTag).not.toBe(null);
 })
 
-test("Create an image inside the .postcard-header div", function(){
+test("Create an image inside the .postcard-header <div>", function(){
     const e=dom.querySelector('.postcard-header').inside().querySelector('img')
     expect(e).toBeTruthy();
 })
 
-test("Create an heading 1 inside the .postcard-header div", function(){
+test("Create an <h1> inside the .postcard-header <div>", function(){
     const e=dom.querySelector('.postcard-header').inside().querySelector('h1')
     expect(e).toBeTruthy();
 })
 
-test("The h1 inside postcard header should say 'My Postcard'", function(){
+test("The <h1> inside .postcard-header should say 'My Postcard'", function(){
     const e =dom.querySelector('.postcard-header').inside().querySelector('h1')
     expect(e.innerHTML.toLowerCase()).toEqual("My Postcard".toLowerCase());
 })
 
-test("The image tag should contain the given URL as src, E.g: <img src='the_image_src_goes_here' />", function(){
+test("The <img> tag should contain the given URL as src, E.g: <img src='the_image_src_goes_here' />", function(){
     const e =dom.querySelector('.postcard-header').inside().querySelector('img')
     expect(e.src).toEqual('https://raw.githubusercontent.com/breatheco-de/exercise-postcard/e63a7916530cc850bd92aa1c2e19191837fb5c80/.learn/assets/4geeks.png');
 })
